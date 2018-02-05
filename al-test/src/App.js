db0900095fe86c8bf7d017b6e324373f615bdf12
Rui <rui.albuquerque.dorey@gmail.com>
2018-02-05 23:36:40 +0000

    icons and html image update

--- al-test/src/App.js
+++ al-test/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './assetslux.png';
+
 import './App.css';
 import { RendererTest } from './webgl/RendererTest';
 
@@ -10,7 +12,7 @@ class App extends Component {
                 <div className="App">
                     <header className="App-header">
                         <img src={logo} className="App-logo" alt="logo"/>
-                        <h1 className="App-title">Welcome to React</h1>
+                        <h1 className="App-title">Welcome to Asets-Lux TEST</h1>
                     </header>
                     <p className="App-intro">
                         To get started, edit <code>src/App.js</code> and save to reload. </p>
